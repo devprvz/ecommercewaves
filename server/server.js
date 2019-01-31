230d0808ea51ca692a0c3b1445a7e22207c4157c
Miraniparvez Khojamusalman <mirani.parvez@outlook.co.nz>
2019-01-31 23:16:46 +1300

    add server

--- server/server.js
+++ server/server.js
@@ -10,11 +10,13 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.json({ extended: true }));
 app.unsubscribe(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -445,6 +447,14 @@ app.post('/api/site/site_data',auth,admin,(req,res)=>{
         }
     )
 })
+
+//DEFAULT
+if(process.env.NODE_ENV==='production'){
+    const path=require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path,resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3002;
 
